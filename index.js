5d83218f7e402c3ec7e40c4372e1297393733b43
Follett <clf9008@gmail.com>
2020-12-13 12:18:45 -0700

    updated start prompt from function to const

--- index.js
+++ index.js
@@ -19,50 +19,94 @@ connection.connect(err => {
 });
 
 //Prompt that will display upon running application 
-function startPrompt() {
-    inquirer.prompt([
-        {
-            type: "list",
-            message: "What would you like to do?",
-            choices: [
-                "View All Employees?", 
-                "View All Employee's By Roles?",
-                "View all Emplyees By Deparments", 
-                "Update Employee",
-                "Add Employee?",
-                "Add Role?",
-                "Add Department?"
-            ]
+const promptUser = () => {
+    inquirer.prompt ([
+      {
+        type: 'list',
+        name: 'choices', 
+        message: 'What would you like to do?',
+        choices: ['View all departments', 
+                  'View all roles', 
+                  'View all employees', 
+                  'Add a department', 
+                  'Add a role', 
+                  'Add an employee', 
+                  'Update an employee role',
+                  'Update an employee manager',
+                  "View employees by department",
+                  'Delete a department',
+                  'Delete a role',
+                  'Delete an employee',
+                  'View department budgets',
+                  'No Action']
       }
-    ]).then(function(val) {
-        switch (val.choice) {
-            case "View All Employees?":
-              viewAllEmployees();
-            break;
-    
-          case "View All Employee's By Roles?":
-              viewAllRoles();
-            break;
-          case "View all Emplyees By Deparments":
-              viewAllDepartments();
-            break;
-          
-          case "Add Employee?":
-                addEmployee();
-              break;
-
-          case "Update Employee":
-                updateEmployee();
-              break;
-      
-            case "Add Role?":
-                addRole();
-              break;
-      
-            case "Add Department?":
-                addDepartment();
-              break;
-    
-            }
-    })
+    ])
+      .then((answers) => {
+        const { choices } = answers; 
+  
+        if (choices === "View all departments") {
+          showDepartments();
+        }
+  
+        if (choices === "View all roles") {
+          showRoles();
+        }
+  
+        if (choices === "View all employees") {
+          showEmployees();
+        }
+  
+        if (choices === "Add a department") {
+          addDepartment();
+        }
+  
+        if (choices === "Add a role") {
+          addRole();
+        }
+  
+        if (choices === "Add an employee") {
+          addEmployee();
+        }
+  
+        if (choices === "Update an employee role") {
+          updateEmployee();
+        }
+  
+        if (choices === "Update an employee manager") {
+          updateManager();
+        }
+  
+        if (choices === "View employees by department") {
+          employeeDepartment();
+        }
+  
+        if (choices === "Delete a department") {
+          deleteDepartment();
+        }
+  
+        if (choices === "Delete a role") {
+          deleteRole();
+        }
+  
+        if (choices === "Delete an employee") {
+          deleteEmployee();
+        }
+  
+        if (choices === "View department budgets") {
+          viewBudget();
+        }
+  
+        if (choices === "No Action") {
+          connection.end()
+      };
+    });
+  };
+//View All Employees function
+function viewAllEmployees() {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;", 
+    function(err, res) {
+      if (err) throw err
+      console.table(res)
+      startPrompt()
+  })
 }
