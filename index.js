c5b1cc76c532567593cb6ecda8608c610480b96c
Follett <clf9008@gmail.com>
2020-12-20 20:10:36 -0700

    updated index.js with addRoles function

--- index.js
+++ index.js
@@ -115,3 +115,54 @@ function addDepartment() {
     })
 }
 
+function addRole() {
+    connection.query('SELECT * FROM department', function(err, res) {
+        if (err) throw (err);
+        inquirer
+        .prompt([{
+            name: "title",
+            type: "input",
+            message: "What is the title of the new role?",
+          }, 
+          {
+            name: "salary",
+            type: "input",
+            message: "What is the salary of the new role?",
+          },
+          {
+            name: "departmentName",
+            type: "list",
+            message: "Which department does this role fall under?",
+            choices: function() {
+                var choicesArray = [];
+                res.forEach(res => {
+                    choicesArray.push(
+                        res.name
+                    );
+                })
+                return choicesArray;
+              }
+          }
+          ])
+          .then(function(answer) {
+            const department = answer.departmentName;
+            connection.query('SELECT * FROM DEPARTMENT', function(err, res) {
+            
+                if (err) throw (err);
+             let filteredDept = res.filter(function(res) {
+                return res.name == department;
+            }
+            )
+            let id = filteredDept[0].id;
+           let query = "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)";
+           let values = [answer.title, parseInt(answer.salary), id]
+           console.log(values);
+            connection.query(query, values,
+                function(err, res, fields) {
+                console.log(`You have added this role: ${(values[0]).toUpperCase()}.`)
+                })
+                viewRoles()
+            })
+        })
+    })
+}
